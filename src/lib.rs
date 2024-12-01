6c86ae536461348da228669200db60b3ff4711a6
hp <morehardy@163.com>
2024-12-01 20:10:14 +0800

    feat: lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 mod error;
 mod utils;
 mod storage;
+mod builders;
 
 pub use error::AnkiError;
+
+pub use builders::Note;
+pub use builders::Field;
+pub use builders::Template;
+
 pub type Result<T> = std::result::Result<T, AnkiError>;
+
+pub use utils::{generate_guid};
+
